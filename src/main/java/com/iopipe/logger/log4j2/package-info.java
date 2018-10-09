7cd3535aefbc6094956455d5ddd09d03f30de2ce
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-10-09 15:39:46 -0400

    Base package and test classes.

---
+++ src/main/java/com/iopipe/logger/log4j2/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains the Log4j2 adapter.
+ *
+ * @since 2018/10/09
+ */
+
+package com.iopipe.logger.log4j2;
---
+++ src/test/java/com/iopipe/logger/log4j2/Log4j2Test.java
@@ -0,0 +1,26 @@
+package com.iopipe.logger.tinylog;
+
+import com.iopipe.IOpipeService;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Test for Log4j2
+ *
+ * @since 2018/10/09
+ */
+public class Log4j2Test
+{
+	/**
+	 * Tests the logging.
+	 *
+	 * @since 2018/10/09
+	 */
+	@Test
+	public void test()
+	{
+		throw new Error("TODO");
+	}
+}
+
